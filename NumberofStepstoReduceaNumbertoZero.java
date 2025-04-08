710f502da1352c99aa542dece916600774267765
anandavii <anandavii@gmail.com>
2025-04-08 18:57:18 -0500

    updated readme

---
+++ NumberofStepstoReduceaNumbertoZero.java
@@ -0,0 +1,21 @@
+public class NumberofStepstoReduceaNumbertoZero {
+    class Solution {
+        public int numberOfSteps(int num) {
+            int steps=0;
+
+            while(num > 0){
+                if(num%2 == 0){
+                    num = num/2;
+
+                }
+                else{
+                    num = num-1;
+
+                }
+                steps++;
+            }
+            return steps;
+        }
+    }
+}
+
