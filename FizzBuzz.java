47dc76236a3259160b5434c3a075f4fe1004e6e4
anandavii <anandavii@gmail.com>
2025-04-08 19:17:29 -0500

    Auto-commit hello.java on 2025-04-08 19:17:29

---
+++ FizzBuzz.java
@@ -0,0 +1,22 @@
+
+    class Solution {
+        public List<String> fizzBuzz(int n) {
+            List<String> answer = new ArrayList<>(n);
+
+            for (int i = 1 ; i <=n ; i++){
+                boolean divby3 = i % 3 == 0;
+                boolean divby5 = i % 5 == 0;
+
+                if(divby3 && divby5)
+                    answer.add("FizzBuzz");
+                else if (divby3)
+                    answer.add("Fizz");
+                else if (divby5)
+                    answer.add("Buzz");
+                else
+                    answer.add(String.valueOf(i));
+            }
+            return answer;
+        }
+    }
+
