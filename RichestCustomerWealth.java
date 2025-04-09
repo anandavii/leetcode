9ee72b0dfc5905a56d10219b7ff092bb5018eece
anandavii <anandavii@gmail.com>
2025-04-08 21:19:58 -0500

    Auto-commit RichestCustomerWealth.java on 2025-04-08 21:19:58

--- RichestCustomerWealth.java
+++ RichestCustomerWealth.java
@@ -1,3 +1,19 @@
 public class RichestCustomerWealth {
-    
+    class Solution {
+        public int maximumWealth(int[][] accounts) {
+            int maxWealthSoFar = 0;
+
+            for(int[] customer : accounts){
+                int currentCustomerWealth = 0;
+
+                for (int bank : customer){
+                    currentCustomerWealth += bank;
+                }
+
+                maxWealthSoFar = Math.max(currentCustomerWealth,maxWealthSoFar);
+            }
+            return maxWealthSoFar;
+        }
+    }
+
 }
