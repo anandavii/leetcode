802e3fd7ddc802b66a51c4aa5bb58cd911346efd
anandavii <anandavii@gmail.com>
2025-04-08 20:16:41 -0500

    Auto-commit RunningSumof1dArray.java on 2025-04-08 20:16:41

--- RunningSumof1dArray.java
+++ RunningSumof1dArray.java
@@ -1,2 +1,14 @@
 public class RunningSumof1dArray {
+    class Solution {
+        public int[] runningSum(int[] nums) {
+            int[] newArr = new int[nums.length];
+            newArr[0] = nums[0];
+
+            for(int i =1; i<nums.length ; i++){
+                newArr[i] = nums[i] + newArr[i-1];
+            }
+            return newArr;
+
+        }
+    }
 }
