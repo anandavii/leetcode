faf267a24fdcf348f715b7ba9792b84240a48559
anandavii <anandavii@gmail.com>
2025-04-09 22:51:51 -0500

    📦 Auto-commit MiddleOfTheLinkedList.java + README update on 2025-04-09 22:51:51

--- MiddleOfTheLinkedList.java
+++ MiddleOfTheLinkedList.java
@@ -1,4 +1,25 @@
 // https://leetcode.com/problems/middle-of-the-linked-list/description/
 public class MiddleOfTheLinkedList {
-    
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode() {}
+     *     ListNode(int val) { this.val = val; }
+     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        public ListNode middleNode(ListNode head) {
+            ListNode middle = head;
+            ListNode end = head;
+
+            while(end != null && end.next != null){
+                middle = middle.next;
+                end = end.next.next;
+            }
+            return middle;
+        }
+    }
 }
